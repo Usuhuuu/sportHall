1a307c09d7d25ab82ac6b14215df5f3a475d4031
과제2.c <usuhuu420@gmail.com>
2024-08-08 17:27:02 +0900

    push hiiv

--- model/dataModel.js
+++ model/dataModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const {Schema, Types} = mongoose
+
 const userSchema =  new Schema({
-    userID : {
+    userName_ID : {
         type: String,
         require: true,
         unique:true
@@ -10,12 +11,58 @@ const userSchema =  new Schema({
         type:String,
         required: true,
     },
+    phoneNumber:{
+        type: Number,
+        require: true
+    },
     password: {
         type: String,
         require:true
+    },
+    userType:{
+        type: String,
+        enum: ['admin', 'user', 'contracter', 'guest'],
+        require: true
     }
 });
-
-
 const User = mongoose.model('User', userSchema);
-module.exports = {User}
+
+const userNames = new Schema({
+    userName_ID:{
+        type:Schema.Types.ObjectId,
+        require:true,
+        ref: "User"
+    },
+    firstName:{
+        type:String,
+        require:true,
+    },
+    lastName:{
+        type:String,
+        required: true,
+    },
+})
+const UserNames = mongoose.model("User_Names", userNames)
+
+const userAgreeSchema = new Schema({
+    userName_ID:{
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: "User"
+    },
+    agree_terms:{
+        type: Boolean,
+        require: true,
+        null: false
+    },
+    agree_privacy:{
+        type: Boolean,
+        require: true,
+        null: false
+    }
+})
+
+const UserAgreements = mongoose.model("UserAgreeterms", userAgreeSchema)
+
+
+module.exports = {User, UserAgreements, UserNames}
--- server.js
+++ server.js
@@ -1,35 +1,44 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
-const connectDB = require('./config/dbConnect.js')
-const app = express()
+const connectDB = require('./config/dbConnect.js');
+const app = express();
 const helmet = require('helmet');
-const cors = require('cors')
+const cors = require('cors');
+
 connectDB();
 
-//Router define
+//middleWare 
 app.use(helmet.hsts({
     maxAge:31536000,
     includeSubDomains: true,
     preload:true
 }))
+const allowedOrigins = ["https://192.168.0.6:8081", 'http://exp://192.168.0.6:8081', 'exp://192.168.0.6:8081'];
+const corsOption = {
+    origin: (origin, callback) => !origin || allowedOrigins.indexOf(origin) !== -1 ? callback(null, true) : callback(new Error("Not allowed by CORS")),
+    credentials:true,
+}
+app.use(cors(corsOption));
+app.use(express.json())
 
+const setCorsHeader =(req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+}
+app.use(setCorsHeader);
+
+//Router define
 const signup = require("./route/signup.js")
-const login = require("./route/signup.js")
+const login = require("./route/login.js")
 const profile = require("./route/profile.js")
 
 app.use("/auth",signup,)
 app.use("/auth", profile)
 app.use("/auth", login)
 
-
-//cors 
-const corsOption = {
-    credentials:true,
-    //origin
-}
-app.use(cors(corsOption))
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`server is running port ${process.env.PORT}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server is running port ${PORT}`)
 })
